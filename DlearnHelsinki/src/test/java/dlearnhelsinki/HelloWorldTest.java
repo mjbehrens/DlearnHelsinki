dc486fada4f2296b515bb322f755b16e899b74c9
Wadelma <delma@del.ma>
2017-09-09 20:05:43 +0300

    coveralls and hello world test

---
+++ DlearnHelsinki/src/test/java/dlearnhelsinki/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package dlearnhelsinki;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author delma
+ */
+public class HelloWorldTest {
+    
+    public HelloWorldTest() {
+    }
+    
+    @Before
+    public void setUp() {   
+    }
+    
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [![Build Status](https://travis-ci.org/TheSoftwareFactory/DlearnHelsinki.svg?branch=master)](https://travis-ci.org/TheSoftwareFactory/DlearnHelsinki)
+[![Coverage Status](https://coveralls.io/repos/github/TheSoftwareFactory/DlearnHelsinki/badge.svg?branch=master)](https://coveralls.io/github/TheSoftwareFactory/DlearnHelsinki?branch=master)

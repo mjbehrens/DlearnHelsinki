fe8da1833a82e0991e464de67a884cc4381ca9a4
Hlad Nicolas <nicolas.hlad@etu.umontpellier.fr>
2017-09-12 17:25:29 +0300

    Working HelloWorld native app for first iteration

---
+++ nativeScriptApp/HelloWorld/app/shared/view-models/user-database.js
@@ -0,0 +1,35 @@
+var config = require("../../shared/config");
+var fetchModule = require("fetch");
+var observableModule = require("data/observable");
+
+
+function User(info) {
+    
+        // You can add properties to observables on creation
+        let viewModel = new observableModule.fromObject();
+
+        viewModel.putScore = function() {
+            return fetchModule.fetch(config.apiUrl, {
+                method: "PUT",
+                body: JSON.stringify({
+                    student: "1",
+                    spidergraphs: "1"
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(handleErrors);
+        };
+
+        return viewModel;
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+        console.log(JSON.stringify(response));
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+module.exports = User;

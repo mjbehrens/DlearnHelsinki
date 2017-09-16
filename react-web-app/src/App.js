586d05df9a4406a70c1c849ba1589f30fa3e881d
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-09-16 20:58:37 +0300

    implement functional folder structure and app skeleton

--- react-web-app/src/App.js
+++ react-web-app/src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SpiderGraph from './SpiderGraph.js';
+import './css/App.css';
+
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
+import SpiderGraph from './components/SpiderGraph.js';
 
 class App extends Component {
   render() {
   
     return (
       <div classsName="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p>
-        </p>
+        <Header />
         <SpiderGraph />
+        <Footer />
       </div>
     );
   }

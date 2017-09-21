0ef2db2c0df7c7d85894ea2085a6c543facc53e4
Hlad Nicolas <nicolas.hlad@etu.umontpellier.fr>
2017-09-21 18:44:31 +0300

    stable version with working survey page for student

--- react-web-app/src/pages/StudentSurveyQuestion.js
+++ react-web-app/src/pages/StudentSurveyQuestion.js
@@ -3,53 +3,106 @@ import React, { Component } from 'react';
 import Footer from '../components/Footer.js';
 import Header from '../components/Header.js';
 import Slider, { Range } from 'rc-slider';
+import {Redirect} from 'react-router'
 import 'rc-slider/assets/index.css';
 
+
+const style = { width: 600, margin: 50, textAlign : 'center' };
+
+function log(value) {
+    console.log(value); //eslint-disable-line
+  }
 
 class StudentSurveyQuestion extends Component {
 
     constructor(props){
         super(props);
         this.state = {
+            buttonValue : 'Next',
+            redirect : false,
             index : 0,
             questions : [],
-            currentQuestion : ""
+            currentQuestion : "",
+            startPoint : 3
         }
     }
+     
 
     componentDidMount(){
-        this.questions = getQuestions();
-        this.index = 0;
-        this.setState({...this.state,
-            this.state.currentQuestion : this.questions}) 
+        this.setState({...this.state.currentQuestion = this.getQuestions()[this.state.index]}) 
     }
 
+    
     getQuestions = function () {
         // do the REST code here to GET questions
         return ["I presented my own viewpoints in the group",
-                 "I listening other's opinions", 
-                 "I argued my own perspective in group discussions"]
+                "I listening other's opinions", 
+                "I argued my own perspective in group discussions",
+                "I tried to understand others ideas",
+                "I gave feedback of the developments for others",
+                "I participated actively to the groupework",
+                "I took enought responsibility of the groupwork",
+                "I helped others in the challenges of the groupwork",
+                "I received useful feedback in the group to continue the work",
+                "I focused completely on doing the groupwork",
+                "I took others ideas into account in the groupwork",
+                "Everyone participated equally in the groupwork",
+                "I worked with others who could help",
+                "I encouraged our group in doing the collaborative task"]
+    }
+    
+    onSliderChange = (value) => {
+        this.setState({
+          ...this.state.startPoint = value,
+        });
+      }
+
+      onAfterChange = (value) => {
+      
+    }
+
+    onClickNext = () =>{
+              
+        if (this.state.index < this.getQuestions().length){
+            this.state.index =  this.state.index + 1;
+            this.setState({...this.state,
+                            currentQuestion : this.getQuestions()[this.state.index]});      
+            //for the last click on the button
+            if(this.state.index == this.getQuestions().length){
+                this.setState({... this.state,
+                        buttonValue : 'Quit',
+                        currentQuestion : 'You have finished the survey'});
+            }
+        }else{
+            this.setState({...this.state.redirect = true})
+        }
+
     }
 
     render() {
 
-
-        var question = "";
-        var questions = [];
-        questions = this.getQuestions();
-
-        return (
-            <div className="text-center">
-              <Header />
-              <h1>Welcome Tobi!</h1>
-               
-                <p> { this.question } </p>
-                <Slider min={1} max={5} defaultValue={2} />
-                <button type="button" className="btn btn-primary">NEXT</button>
-
-              <Footer />
-            </div>
-          );
+        if(this.state.redirect){
+            return <Redirect to="/student-dashboard"/>
+        }else{
+            return (
+                <div style={style}>
+                  <Header />
+    
+                    <p> { this.state.currentQuestion } </p>
+                    <Slider min={1} max={5} dots={true} 
+                            value={this.state.startPoint} 
+                            onChange={this.onSliderChange}
+                            onAfterChange={this.onAfterChange}/>
+                    
+                    <span>{ this.state.startPoint }/5 </span>
+                    <button type="button" 
+                            className="btn btn-primary"
+                            onClick={this.onClickNext}>{ this.state.buttonValue }</button>
+                  
+                  <Footer />
+                </div>
+              );
+        }
 
     }
 }

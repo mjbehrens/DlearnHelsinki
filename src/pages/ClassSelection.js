c19bc6ff82143681f0f775b424c85aafecd8c4d7
MarEf <maria.efimova93@gmail.com>
2017-09-22 17:11:10 +0300

    work in progress

--- src/pages/ClassSelection.js
+++ src/pages/ClassSelection.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
+var tempList = ["Math 1", "Math 2", "Math 5", "Math 8", "Math 12", "Math 21"];
+
+
 class ClassSelection extends Component {
     
     render() {
+        
+        
         return (
             <div className="App">
                 <h1>Select a Class</h1> 
                 <div className="row">**Yeah, this implementation is a "No"... Think up something else**
+                    
+                    <div>
+                    
+                    
+                    </div>
                     <div className="btn-toolbar">
                         <button type="button" className="btn btn-primary">Math 1</button>
                         <button type="button" className="btn btn-primary">Math 2</button>

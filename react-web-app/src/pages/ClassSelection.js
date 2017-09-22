b9b74c2eea14eb420702ff0729727fac61f43b6d
MarEf <maria.efimova93@gmail.com>
2017-09-22 12:13:33 +0300

    adding margins to buttons

--- react-web-app/src/pages/ClassSelection.js
+++ react-web-app/src/pages/ClassSelection.js
@@ -5,14 +5,14 @@ class ClassSelection extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Select a Class</h1>
-                <div className="row">
-                    <div className="btn-group-horisontal">
+                <h1>Select a Class</h1> 
+                <div className="row">Yeah, this implementation is a "No"... Think up something else
+                    <div className="btn-toolbar-vertical">
                         <button type="button" className="btn btn-primary">Math 1</button>
                         <button type="button" className="btn btn-primary">Math 2</button>
                         <button type="button" className="btn btn-primary">Math 5</button>
                     </div>
-                    <div className="btn-group-horisontal">
+                    <div className="btn-toolbar-vertical">
                         <button type="button" className="btn btn-primary">Math 8</button>
                         <button type="button" className="btn btn-primary">Math 12</button>
                         <button type="button" className="btn btn-primary">Math 21</button>

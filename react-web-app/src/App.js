cdb72672fe80ee110e3c261f688d323bee2604d7
Hlad Nicolas <nicolas.hlad@etu.umontpellier.fr>
2017-09-22 15:19:48 +0300

    change page name

--- react-web-app/src/App.js
+++ react-web-app/src/App.js
@@ -6,6 +6,8 @@ import Footer from './components/Footer.js';
 import Header from './components/Header.js';
 import Home from './pages/Home.js';
 import StudentDashboard from './pages/StudentDashboard.js';
+
+
 import StudentSurveyQuestion from './pages/StudentSurveyQuestion.js'
 
 class App extends Component {
@@ -15,11 +17,10 @@ class App extends Component {
       <div classsName="App">
         <Header />
         <Switch>
-		  <Route exact path='/' component={Home}/>
-		  <Route path='/student-dashboard' component={StudentDashboard}/>
-      <Route path='/student-survey' component={StudentSurveyQuestion}/>
-
-		</Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/student-dashboard' component={StudentDashboard}/>
+          <Route path='/student-survey' component={StudentSurveyQuestion}/>
+		    </Switch>
         <Footer />
       </div>
     );

82f699b806489cf37e87fd2f2109559b3e8a4577
MarEf <maria.efimova93@gmail.com>
2017-09-22 16:16:25 +0300

    minor edits

--- src/pages/ClassSelection.js
+++ src/pages/ClassSelection.js
@@ -7,12 +7,12 @@ class ClassSelection extends Component {
             <div className="App">
                 <h1>Select a Class</h1> 
                 <div className="row">**Yeah, this implementation is a "No"... Think up something else**
-                    <div className="btn-toolbar-vertical">
+                    <div className="btn-toolbar">
                         <button type="button" className="btn btn-primary">Math 1</button>
                         <button type="button" className="btn btn-primary">Math 2</button>
                         <button type="button" className="btn btn-primary">Math 5</button>
                     </div>
-                    <div className="btn-toolbar-vertical">
+                    <div className="btn-toolbar">
                         <button type="button" className="btn btn-primary">Math 8</button>
                         <button type="button" className="btn btn-primary">Math 12</button>
                         <button type="button" className="btn btn-primary">Math 21</button>
--- src/pages/TeacherDashboard.js
+++ src/pages/TeacherDashboard.js
@@ -7,10 +7,18 @@ class TeacherDashboard extends Component {
     
     render() {
         return (
-            <div className="App">
+            <div className = "App">
                 <h1>Latest Survey</h1> 
-                Box for survey data
-                
+                <div className = "Latest-results">
+                    <div className = "btn-toolbar">
+                        <button type="button" className="btn btn-primary">Group 1</button>
+                        <button type="button" className="btn btn-primary">Group 2</button>
+                        <button type="button" className="btn btn-primary">Group 3</button>
+                        <button type="button" className="btn btn-primary">Group 4</button>
+                        <button type="button" className="btn btn-primary">Group 5</button>
+                    </div>
+                    Box for survey data
+                </div>
                 <div>
                     <img src={new_survey} className="Icon" alt="icon" />
                     <img src={history} className="Icon" alt="icon" />

856e4fff6d3177b6269c6ad0d06a826714ef4adb
MarEf <maria.efimova93@gmail.com>
2017-09-25 12:57:13 +0300

    Rendering logic done. Now to make it pretty.

---
+++ src/components/teacherCompo/SelectClass.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SelectClass extends React.Component {
+    
+    render(){
+
+        return(
+            <div>
+                <button type="button" 
+                        className="btn btn-primary">
+                            {this.props.nameOfClass}
+                </button>
+            </div>
+        );
+    }
+}
+
+export default SelectClass
--- src/pages/ClassSelection.js
+++ src/pages/ClassSelection.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import SelectClass from '../components/teacherCompo/SelectClass.js';
+
+//Change this part to pull information from the database once it's implemented.
 var tempList = ["Math 1", "Math 2", "Math 5", "Math 8", "Math 12", "Math 21"];
 
 
@@ -9,28 +12,17 @@ class ClassSelection extends Component {
         
         
         return (
-            <div className="App">
+            <div className="SelectClass">
                 <h1>Select a Class</h1> 
-                <div className="row">**Yeah, this implementation is a "No"... Think up something else**
-                    
+
+                    <div className="btn-toolbar">
+                        {tempList.map(function(object, i){
+                             return <SelectClass nameOfClass={object} key={i} />;
+                        })}
+                    </div>
                     <div>
-                    
-                    
+                        <button type = "button" className = "btn btn-default">Add New</button>
                     </div>
-                    <div className="btn-toolbar">
-                        <button type="button" className="btn btn-primary">Math 1</button>
-                        <button type="button" className="btn btn-primary">Math 2</button>
-                        <button type="button" className="btn btn-primary">Math 5</button>
-                    </div>
-                    <div className="btn-toolbar">
-                        <button type="button" className="btn btn-primary">Math 8</button>
-                        <button type="button" className="btn btn-primary">Math 12</button>
-                        <button type="button" className="btn btn-primary">Math 21</button>
-                    </div>
-                    <div className="btn-toolbar-vertical">
-                        <button type="button" className="btn btn-default">Add New</button>
-                    </div>
-                </div>
             </div>
         );
     }

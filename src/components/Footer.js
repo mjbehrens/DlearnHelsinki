93021bcd07cdbde104a8df70873b2ee2fed7941e
MarEf <maria.efimova93@gmail.com>
2017-09-25 16:01:16 +0300

    Added link to UN to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import icon from '../pages/icons/UN-Quality Education.jpg'
 
 export default class Footer extends React.Component {
   render() {
   
     return (
-      <div>
-      </div>
+        <div className = "App">
+            <div className = "App-footer">
+                <Link to = "http://www.un.org/sustainabledevelopment/sustainable-development-goals/"><img src={icon} className="Icon" alt="Sustainable development goals" /></Link>
+            </div>
+        </div>
     );
   }
 }

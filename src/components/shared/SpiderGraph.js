5a5d21fb51f5a57793101277c6280117a97fa2f4
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-09-28 13:07:16 +0300

    update spidergraph

--- src/components/shared/SpiderGraph.js
+++ src/components/shared/SpiderGraph.js
@@ -9,7 +9,7 @@ constructor(props) {
       cpt: 0,
       url: "http://172.20.10.3:8080/skeleton/webapi/students/1/spidergraphs/1",
       data: {
-				labels: ['Value1', 'Value2', 'Value3', 'Value4', 'Value5'],
+	  labels: ['Value1', 'Value2', 'Value3', 'Value4', 'Value5', 'Value6', 'Value7', 'Value8', 'Value9', 'Value10', 'Value11', 'Value12', 'Value13'],
 				datasets: [ {
 					label: 0,
 					backgroundColor: 'rgba(179,181,198,0.2)',
@@ -18,7 +18,7 @@ constructor(props) {
 					pointBorderColor: '#fff',
 					pointHoverBackgroundColor: '#fff',
 					pointHoverBorderColor: 'rgba(179,181,198,1)',
-					data: [0, 2, 5, 8, 4]
+				    data: [3, 2, 5, 2, 4, 6, 2, 2, 3, 2, 5, 4, 7]
 				}
 				]
 			}
@@ -62,24 +62,6 @@ constructor(props) {
     return (
       <div>
         <Radar data={this.state.data} />
-        <p>
-        <button onClick={() => this.setState({...this.state,
-          cpt: this.state.cpt + 1,
-					data: {...this.state.data, datasets: [
-					{
-						label: "Test SpiderGraph",
-						backgroundColor: "rgba(179,181,198,0.2)",
-						borderColor: "rgba(179,181,198,1)",
-						pointBackgroundColor: "rgba(179,181,198,1)",
-						pointBorderColor: "#fff",
-						pointHoverBackgroundColor: "#fff",
-						pointHoverBorderColor: "rgba(179,181,198,1)",
-						data: [this.state.cpt, 2, 5, 8, 4]
-					}
-					]},
-				})}>Click me!</button>
-        <span>{this.state.cpt}</span>
-        </p>
       </div>
     );
   }

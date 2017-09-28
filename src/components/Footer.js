e53be380f45fbde55e51fe1abb5127e48ec59fc2
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-09-28 11:12:07 +0300

    footer always at the bottom fixed!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,9 @@ export default class Footer extends React.Component {
   
     return (
         <footer>
-	    <nav className="navbar navbar-inverse bg-inverse navbar-default navbar-fixed-bottom">
 		<div className="container">
-		   <div id="navbar" className="navbar-collapse collapse">
 			<a href = "http://www.un.org/sustainabledevelopment/sustainable-development-goals/"><img src={icon} className="Icon" alt="Sustainable development goals" /></a>
-		    </div>
 		</div>
-	    </nav>
         </footer>
     );
   }

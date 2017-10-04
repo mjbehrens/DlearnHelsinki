7240f3d041d2d9043b0e10addea8e1c4173515f5
Hlad Nicolas <nicolas.hlad@etu.umontpellier.fr>
2017-10-04 17:50:20 +0300

    merge with master. Solving conflicts

--- src/pages/StudentSurveyQuestion.js
+++ src/pages/StudentSurveyQuestion.js
@@ -9,9 +9,6 @@ const GET_SURVEYS = 'teachers/1/classes/1/surveys/';
 const GET_QUESTIONS_FOR_SURVEY = 'students/1/classes/1/surveys/27/questions';
 const PUT_QUESTION_ANSWER = 'students/1/classes/1/surveys/27/answers/'; //needs one more parameters
 
-const surveyJson = '[{"_id": 1,"question": "I presented my own viewpoints in the group","min_answer": 1,"max_answer": 10},{"_id": 2,"question": "I listening other\'s opinions","min_answer": 1,"max_answer": 5},{"_id": 3,"question": "I tried to understand others ideas","min_answer": 1,"max_answer": 5},{"_id": 4,"question": "I gave feedback of the developments for others","min_answer": 1,"max_answer": 5},{"_id": 5,"question": "I participated actively to the groupework","min_answer": 1,"max_answer": 5},{"_id": 6,"question": "I took enough responsibility of the groupwork","min_answer": 1,"max_answer": 5},{"_id": 7,"question": "I helped others in the challenges of the groupwork","min_answer": 1,"max_answer": 5},{"_id": 8,"question": "I received useful feedback in the group to continue the work","min_answer": 1,"max_answer": 5},{"_id": 9,"question": "I focused completely on doing the groupwork","min_answer": 1,"max_answer": 5},{"_id": 10,"question": "I took others ideas into account in the groupwork","min_answer": 1,"max_answer": 5},{"_id": 11,"question": "Everyone participated equally in the groupwork","min_answer": 1,"max_answer": 5},{"_id": 12,"question": "I worked with others who could help","min_answer": 1,"max_answer": 5},{"_id": 13,"question": "I encouraged our group in doing the collaborative task","min_answer": 1,"max_answer": 5}]';
-
-
 class StudentSurveyQuestion extends Component {
 
     constructor(props) {
@@ -24,7 +21,7 @@ class StudentSurveyQuestion extends Component {
             survey: [],
             currentQuestion: {
                 id: 0,
-                question: 'NaN',
+                question: 'Loading the survey...',
                 min_answer: 0,
                 max_answer: 0,
             },

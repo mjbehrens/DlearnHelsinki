fb4511a3c5961516b901be9c1d08aa3166edfb86
MarEf <maria.efimova93@gmail.com>
2017-10-12 15:49:26 +0300

    Nearly ready for integration

--- src/pages/ClassSelection.js
+++ src/pages/ClassSelection.js
@@ -5,29 +5,51 @@ import SelectClass from '../components/teacherCompo/SelectClass.js';
 var classesJSON = '[{"_id": 1, "className": "Math 1"}, {"_id": 2, "className": "Math 2"}, \n\
 {"_id": 3, "className": "Math 5"}, {"_id": 4, "className": "Math 8"}, {"_id": 12, "className": "Math 9"},\n\
  {"_id": 6, "className": "Math 21"}, {"_id": 7, "className": "Homeroom"}]';
+var classes;
 
+var ORIGIN = "https://dlearn-helsinki-backend.herokuapp.com/webapi";
+var GET_CLASSES = "";
 
 class ClassSelection extends Component {
 
     constructor() {
         super();
-//        this.state = {
-//            selectedClass: null
-//        }
+        var classes = [];
     }
 
-//    moveToDashboard = () => {
-//        if (this.state.selectedClass != null) {
-//            this.props.history.push({
-//                pathname: '/teacher-dashboard',
-//                state: {className: this.state.selectedClass}  
-//            })
-//        }
-//    }
+    // There are no classes implemented in the database, so it's currently impossible
+    // to test this function.
+    buildRequestREST = function() {
+        var s = '';
+        s = s + '/teachers/1/classes/'; // Warning! Hard coded for testing purposes.
+        GET_CLASSES = s;
+    }
+    // There are no classes implemented in the database, so it's currently impossible
+    // to test this function.
+    getClassesREST = function() {
+        console.log(ORIGIN + GET_CLASSES);
+        fetch(ORIGIN + GET_CLASSES, {
+            method: "GET",
+            headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Authorization': 'Basic ' + btoa('teacher:password')
+            }
+        }).then(function(response) {
+            if(response.ok) {
+                response.json().then(data => {
+                                    classes = data;
+				});
+            }else {
+		console.log('Network response was not ok.');
+            }
+        }).catch(function (err) {
+            // Error
+            console.log(err);
+	});
+    }
 
     selectClass = (buttonValue) => (e) => {
         e.preventDefault();
-//        this.setState({selectedClass: buttonValue}); console.log("Changing state...")
         this.props.history.push({
                 pathname: '/teacher-dashboard',
                 state: {className: buttonValue}
@@ -37,25 +59,20 @@ class ClassSelection extends Component {
     renderButton(buttonValue) {
         return(
                 <button style = {{margin: "1vmin"}}
-                        onClick = {this.selectClass(buttonValue)}
+                        key = {buttonValue._id}
+                        onClick = {this.selectClass(buttonValue.className)}
                         className="btn btn-primary">
-                            {buttonValue}
+                            {buttonValue.className}
                 </button>
         );
     }
     
     render() {
-        var parsed_classes = JSON.parse(classesJSON);
-        var class_list = [];
+        var class_list = JSON.parse(classesJSON);;
         var temp = 0;
-        parsed_classes.forEach(function(e){
-           class_list[temp] = e.className;
-           temp = temp+1;
-        });
         
-        var buttons = class_list.map(function(className, i){
-//                             return <SelectClass nameOfClass={object} key={i} />; //OBSOLETE
-                            return this.renderButton(className);
+        var buttons = class_list.map(function(classInList, i){
+                            return this.renderButton(classInList);
                         }, this);
         
         return (

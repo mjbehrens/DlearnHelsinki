622caa6c26b463e8754a9a9904708a4ec5c425c4
alkalinan <nicolas.hlad@etu.umontpellier.fr>
2017-10-12 11:57:55 +0300

    Revert "Revert "Teacher dashboard""

--- src/components/shared/HistoryDisplay.js
+++ src/components/shared/HistoryDisplay.js
@@ -4,38 +4,50 @@ const buttonStyle = {
     margin: '5px'
 }
 
+var compo = null;
 class HistoryDisplay extends Component {
-    
+
     constructor(props) {
         super(props);
-    }
-//    
-//    shouldComponentUpdate(nextProps, nextState) {       
-//        return nextProps.searchData !== this.props.searchData; 
-//    } 
-  
-  componentWillReceiveProps(nextProps) {
-        if (this.props != nextProps) {
 
-            this.setState({...this.state,
-            	searchData: nextProps.searchData
+        compo = this;
+
+        this.state = {
+            searchData: [],
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props != nextProps) {
+            this.setState({
+                //...this.state,
+                searchData: nextProps.searchData
             });
         }
-  }
-    
+    }
+
+    onClickSurvey = function (surveyId) {
+        // TODO : change color of the button 
+        compo.props.loadResult(surveyId);
+    }
+
     render() {
         var searchResults = [];
-        let compo = this;
         let i = 0;
-        this.props.searchData.forEach( function(result) {
-            if (true) {
-                searchResults.push(<button key = {i} className="btn btn-default left-align" onClick = {compo.props.loadResult(result._id)}> {result.start_date +" "+ result.testInput} </button>)
-                i = i+1;
+        this.state.searchData.forEach(function (result) {
+            if (true) { // seriously ???? 
+                searchResults.push(<button key={i}
+                    className="btn btn-default left-align"
+                    onClick={compo.onClickSurvey.bind(compo, result._id)}>
+                    {result.start_date + " " + result.title}
+                </button>)
+                i = i + 1;
             }
         });
-        return(
-            <div style = {buttonStyle} className="btn-group-vertical">
-                { searchResults }
+
+        return (
+            <div style={buttonStyle} className="btn-group-vertical">
+                {searchResults}
             </div>
         )
     }

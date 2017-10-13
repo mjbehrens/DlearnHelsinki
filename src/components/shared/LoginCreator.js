75214eeea07018fb8cda62d327e20ffb6b59745d
MarEf <maria.efimova93@gmail.com>
2017-10-13 10:59:24 +0300

    password guidelines

--- src/components/shared/LoginCreator.js
+++ src/components/shared/LoginCreator.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
+// About password guidelines: https://pages.nist.gov/800-63-3/sp800-63b.html
+// TL;DR: https://nakedsecurity.sophos.com/2016/08/18/nists-new-password-rules-what-you-need-to-know/
+
 const margins = {
     margin: '5px'
-}
-
-const formStyle = {
-    width: '50%'
 }
 
 class LoginCreator extends Component {
@@ -63,35 +62,56 @@ class LoginCreator extends Component {
         this.selectNumber;
         
         this.setState ({
-            username: (""+this.state.adj + this.state.adv + this.state.noun + this.state.nbr)
+            username: (""+this.state.adj +" "+this.state.adv +" "+ this.state.noun +" "+ this.state.nbr)
         });
     }
 
     render() {
         this.genedateUsername;
         console.log("Username: "+this.state.username);
+        
         return(
-            <div className = "form-group" style = {formStyle}>
-                <label for="uName">Username:</label>
-                <input type="text"
-                    className="form-control"
-                    style = {margins}  
-                    id = "uName"
-                    value = {this.state.username}
-                    required
-                    />
-                <label for="password">Password:</label>
-                <input type="text" 
-                    className="form-control"
-                    style = {margins}  
-                    id = "password"
-                    required
-                    />
-                <div>
-                    <button className="btn btn-primary" 
-                        style = {margins}> 
-                            Create Account
-                    </button>
+            <div className ="Login-form">
+                <h1>Create an account</h1>
+                <br></br>
+                <div className = "form-group">
+                    <label for="uName">Choose a username:</label>
+                    <input type="text"
+                        className="form-control"
+                        style = {margins}  
+                        id = "uName"
+                        value = {this.state.username}
+                        required
+                        />
+                    <label for="password">Choose a password:</label>
+                    <input type="text" 
+                        className="form-control"
+                        style = {margins}  
+                        id = "password"
+                        required
+                        />
+                    <div style = {{textAlign: "left"}}>
+                        <i>
+                        <ul>
+                            <li>It is recommended to use a password that is as long as possible. </li>
+                            <li>A password must be at least X characters long. </li>
+                            <li>Be creative with your choice, or use a random word or password generator.</li>
+                            <li>Do not use a username or a real name as the password. </li>
+                            <li>Do not use obvious choices for passwords such as "password", a movie title,
+                            or the name of a sports team in any form in any language. </li>
+                            <li>Avoid using a popularized example of a password, such as "correct horse battery staple",
+                            as an actual password.</li>
+                            <li>If this account is for a child, instruct them to never share their password
+                            with anyone, including their teacher, parent, sibling, or their best friend.</li>
+                        </ul>
+                        </i>
+                    </div>
+                    <div>
+                        <button className="btn btn-primary" 
+                            style = {margins}> 
+                                Create Account
+                        </button>
+                    </div>
                 </div>
             </div>
         )

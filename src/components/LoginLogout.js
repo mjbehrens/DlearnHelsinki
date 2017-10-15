a83b25f93a16b93a8a6ffeb5758b3e92d670cc68
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-10-15 16:43:51 +0300

    Login/Logout button workflow in place

---
+++ src/components/LoginLogout.js
@@ -0,0 +1,33 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import * as userActions from '../actions/userActions';
+
+
+function mapStateToProps(store) {
+    return {
+	user: store.user.user,
+    }
+}﻿
+
+
+class LoginLogout extends React.Component {
+
+    onLoginClick = () => {
+	this.props.dispatch(userActions.logoutUser())
+    }
+
+  render() {
+      if (this.props.user.loggedin) {
+    return (
+	<Link to="/"><button className="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button></Link>
+    );
+      } else {
+    return (
+	<Link to="/login"><button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button></Link>
+    );
+    }
+  }
+}
+
+export default connect(mapStateToProps)(LoginLogout);

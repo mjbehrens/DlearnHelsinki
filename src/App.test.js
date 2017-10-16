d7e4e2561cc35b6bd8884fa10751c4f3970e51a8
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-10-16 17:13:40 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,18 @@
-console.log('Hello world. This is a fake test')
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class TestApp extends Component {
+    render() {
+
+	return (
+	    <div>
+	    <p>This is a basic test component</p>
+		</div>
+		);
+	}
+}
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TestApp />, div);
+});

5b96881df7b34e98035dc5d3857ea24ec6de4fe1
Hlad Nicolas <nicolas.hlad@etu.umontpellier.fr>
2017-10-18 12:14:56 +0300

    fix class selection issue

--- src/pages/ClassSelection.js
+++ src/pages/ClassSelection.js
@@ -11,8 +11,8 @@ import * as classActions from '../actions/classActions';
 
 function mapStateToProps(store) {
     return {
-	user: store.user.user,
-	classes: store.classroom.classes,
+        user: store.user.user,
+        classes: store.classroom.classes,
     }
 }
 
@@ -20,101 +20,115 @@ class ClassSelection extends Component {
 
     constructor(props) {
         super(props);
-	this.state = {
-	    goTo: ROUTES.CLASS_SELECTION,
-	    api: null,
-	    getClassesEndpoint: null,
-	    loading: true,
-	}
+        this.state = {
+            goTo: ROUTES.CLASS_SELECTION,
+            api: null,
+            getClassesEndpoint: '',
+            loading: true,
+        }
         var classes = [];
     }
 
     componentDidMount() {
-	if (this.props.user.type === 'teacher') {
-	    this.setState({
-		...this.state,
-		goTo: ROUTES.TEACHER_DASHBOARD,
-		getClassesEndpoint: 'teachers/' + this.props.user.id + '/classes/',
-	    })
-	} else {
-	    this.setState({
-		...this.state,
-		goTo: ROUTES.STUDENT_DASHBOARD,
-		getClassesEndpoint: 'students/' + this.props.user.id + '/classes/',
-	    })
-	}
+        if (this.props.user.type === 'teacher') {
+            this.setState({
+                ...this.state,
+                goTo: ROUTES.TEACHER_DASHBOARD,
+                getClassesEndpoint: 'teachers/' + this.props.user.id + '/classes/',
+            })
+        } else if (this.props.user.type === 'student'){
+            this.setState({
+                ...this.state,
+                goTo: ROUTES.STUDENT_DASHBOARD,
+                getClassesEndpoint: 'students/' + this.props.user.id + '/classes/',
+            })
+        }
+        this.getClasses();
+
     }
 
     getClasses = () => {
-        fetch(BACKEND_API.ROOT + this.state.getClassesEndpoint, {
+
+        /**
+         * TEMPORAIRE FIX FOR NULL RESQUET 
+         * Otherwise getClassesEndpoint is empty because setstate is a thread 
+         */
+        let getClassesEndpoint = '';
+        if (this.props.user.type === 'teacher') {
+                getClassesEndpoint= 'teachers/' + this.props.user.id + '/classes/';
+        } else if (this.props.user.type === 'student'){
+                getClassesEndpoint= 'students/' + this.props.user.id + '/classes/';
+        }
+
+        fetch(BACKEND_API.ROOT + getClassesEndpoint, {
             method: "GET",
             headers: {
-		'Access-Control-Allow-Origin': '*',
-		'Authorization': 'Basic ' + this.props.user.hash,
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': 'Basic ' + this.props.user.hash,
             }
         }).then((response) => {
-            if(response.ok) {
+            if (response.ok) {
                 response.json().then(data => {
-		    this.props.dispatch(classActions.setClasses(data));
-		});
+                    this.props.dispatch(classActions.setClasses(data));
+                });
             } else {
-		console.log('Network response was not ok.');
+                console.log('Network response was not ok.');
             }
-	    this.setState({
-		...this.state,
-		loading: false,
-	    })
+            this.setState({
+                ...this.state,
+                loading: false,
+            })
         }).catch((err) => {
             console.log(err);
-	});
+        });
     }
 
     selectClass = (buttonValue) => (e) => {
         e.preventDefault();
         this.props.history.push({
-                pathname: this.state.goTo,
-                state: {className: buttonValue}
-            })
+            pathname: this.state.goTo,
+            state: { className: buttonValue }
+        })
     }
 
     renderButton(buttonValue) {
-        return(
-                <button style = {{margin: "1vmin"}}
-                        key = {buttonValue._id}
-                        onClick = {this.selectClass(buttonValue.name)}
-                        className="btn btn-primary">
-                            {buttonValue.name}
-                </button>
+        return (
+            <button style={{ margin: "1vmin" }}
+                key={buttonValue._id}
+                onClick={this.selectClass(buttonValue.name)}
+                className="btn btn-primary">
+                {buttonValue.name}
+            </button>
         );
     }
-    
+
     render() {
-	this.getClasses();
-        var buttons = this.props.classes.map(function(classInList, i){
-                            return this.renderButton(classInList);
-                        }, this);
-        
-	if (this.state.loading) {
-	    return (
-		<div className="SelectClass">
-		    <h1>Select a Class</h1> 
-		    <div className='spinner-container'>
-			<Spinner />
-		    </div>
-		</div>
-	    )
-	} else {
-        return (
-            <div className="SelectClass">
-                <h1>Select a Class</h1> 
+        //this.getClasses();
+        var buttons = this.props.classes.map(function (classInList, i) {
+            return this.renderButton(classInList);
+        }, this);
+
+        if (this.state.loading) {
+            return (
+                <div className="SelectClass">
+                    <h1>Select a Class</h1>
+                    <div className='spinner-container'>
+                        <Spinner />
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="SelectClass">
+                    <h1>Select a Class</h1>
 
                     <div>
                         {buttons}
                     </div>
 
-            </div>
-        );
-	}
+                </div>
+            );
+        }
     }
 }
 // MOVE THIS BACK TO RENDER SHOULD YOU FIND IT MORE APPEALING

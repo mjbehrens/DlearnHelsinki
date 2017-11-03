1ca273c6371e952d0e02ab6319cb74ee32ed3aa6
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-11-03 12:02:56 +0200

    Fix #105

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../constants.js';
+
+function mapStateToProps(store) {
+    return {
+	user: store.user.user,
+    }
+}
 
 class Home extends Component {
 
@@ -15,16 +22,27 @@ class Home extends Component {
 	
   render() {
   
+      let buttonRedirect = ROUTES.LOGIN
+      let buttonText = 'Login »'
+      if (this.props.user.loggedin) {
+	  if (this.props.user.type === 'teacher') {
+	      buttonRedirect = ROUTES.TEACHER_DASHBOARD
+	  } else {
+	      buttonRedirect = ROUTES.STUDENT_DASHBOARD
+	  }
+	  buttonText = 'Dashboard »'
+      }
+	  
     return (
 	<div className="container">
 	    <div className="jumbotron">
 		<h1>Dlearn.Helsinki</h1>
 		<p>Learning global competencies with new pedagogical tools</p>
-	    <Link to={ROUTES.LOGIN}><button className="btn btn-lg btn-primary" role="button">Login &raquo;</button></Link>
+	    <Link to={buttonRedirect}><button className="btn btn-lg btn-primary" role="button">{buttonText}</button></Link>
 	    </div>
 	</div>
           );
   }
 }
 
-export default Home;
+export default connect(mapStateToProps)(Home);

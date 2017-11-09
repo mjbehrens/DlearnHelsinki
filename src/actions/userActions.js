75dcba1ad7d01550e4349c0c5409e705b2c5e31a
mihassin <marko.hassinen@helsinki.fi>
2017-11-09 14:26:00 +0200

    finished all user action tests

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { ACTION_TYPES as types } from '../constants'
+import { USER_ACTION_TYPES as types } from '../constants'
 
 export function loginUser() {
   return {
@@ -30,35 +30,35 @@ export function setUserName(name) {
 
 export function setUserType(type) {
   return {
-    type: 'SET_USER_TYPE',
+    type: types.SET_USER_TYPE,
     payload: type,
   }
 }
 
 export function setUserLogin(login) {
   return {
-    type: 'SET_USER_LOGIN',
+    type: types.SET_USER_LOGIN,
     payload: login,
   }
 }
 
 export function setUserHash(hash) {
   return {
-    type: 'SET_USER_HASH',
+    type: types.SET_USER_HASH,
     payload: hash,
   }
 }
 
 export function setUserClassId(classid) {
   return {
-    type: 'SET_USER_CLASS_ID',
+    type: types.SET_USER_CLASS_ID,
     payload: classid,
   }
 }
 
 export function setUserGroupId(groupid) {
   return {
-    type: 'SET_USER_GROUP_ID',
+    type: types.SET_USER_GROUP_ID,
     payload: groupid,
   }
 }
--- test/userAction.test.js
+++ test/userAction.test.js
@@ -1,5 +1,5 @@
 import * as actions from '../src/actions/userActions'
-import { ACTION_TYPES as types } from '../src/constants'
+import { USER_ACTION_TYPES as types } from '../src/constants'
 
 describe('actions', () => {
 	it('should create an action for login', () => {
@@ -31,5 +31,46 @@ describe('actions', () => {
 			payload: name
 		}
 		expect(actions.setUserName(name)).toEqual(expectedAction)
-	})		
+	}),
+	it('should create an action to set a type for an user', () => {
+		const type = 'student'
+		const expectedAction = {
+			type: types.SET_USER_TYPE,
+			payload: type
+		}
+		expect(actions.setUserType(type)).toEqual(expectedAction)
+	}),
+	it('should create an action to set a login for an user', () => {
+		const login = true
+		const expectedAction = {
+			type: types.SET_USER_LOGIN,
+			payload: login
+		}
+		expect(actions.setUserLogin(login)).toEqual(expectedAction)
+	}),
+	it('should create an action to set a hash for an user', () => {
+		const hash = 'iddqd'
+		const expectedAction = {
+			type: types.SET_USER_HASH,
+			payload: hash
+		}
+		expect(actions.setUserHash(hash)).toEqual(expectedAction)
+	}),
+	//
+	it('should create an action to set a class id for an user', () => {
+		const classid = 1
+		const expectedAction = {
+			type: types.SET_USER_CLASS_ID,
+			payload: classid
+		}
+		expect(actions.setUserClassId(classid)).toEqual(expectedAction)
+	}),
+	it('should create an action to set a group id for an user', () => {
+		const groupid = 'iddqd'
+		const expectedAction = {
+			type: types.SET_USER_GROUP_ID,
+			payload: groupid
+		}
+		expect(actions.setUserGroupId(groupid)).toEqual(expectedAction)
+	})
 })

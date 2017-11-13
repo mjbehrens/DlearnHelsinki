9f3d5c8705c489f2a8cacdef42631114b1e8e83d
Kalle Lammenoja <kalle.lammenoja@gmail.com>
2017-11-13 12:53:00 +0200

    forgot /

--- src/constants.js
+++ src/constants.js
@@ -15,7 +15,7 @@ export const ROUTES = {
 
 export const BACKEND_API = {
     //ROOT: 'https://dlearn-helsinki-backend.herokuapp.com/webapi/',
-	ROOT: 'http://localhost:8080/webapi',
+	ROOT: 'http://localhost:8080/webapi/',
     GET_STUDENT: ''
 }
 

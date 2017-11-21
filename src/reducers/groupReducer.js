544a9089fb4fc5370b8d3e834deaf12d59f324cf
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-11-21 16:27:49 +0200

    Update group reducer

--- src/reducers/groupReducer.js
+++ src/reducers/groupReducer.js
@@ -1,23 +1,23 @@
-export default function reducer(state={
+const groupInitState = {
     groups: [],
-  }, action) {
+}
+
+export default function reducer(state=groupInitState, action) {
 
     switch (action.type) {
+      case "SET_ALL_GROUPS": {
+	return {
+	  ...state,
+	  groups: action.payload,
+	}
+      }
+      case "DELETE_ALL_GROUPS": {
+        return groupInitState
+      }
       case "ADD_GROUP": {
         return {
           ...state,
           groups: [...state.groups, action.payload],
-        }
-      }
-      case "UPDATE_TWEET": {
-        const { id, text } = action.payload
-        const newTweets = [...state.tweets]
-        const tweetToUpdate = newTweets.findIndex(tweet => tweet.id === id)
-        newTweets[tweetToUpdate] = action.payload;
-
-        return {
-          ...state,
-          tweets: newTweets,
         }
       }
       case "DELETE_GROUP": {
@@ -26,8 +26,17 @@ export default function reducer(state={
           groups: state.groups.filter(group => group.id !== action.payload),
         }
       }
-    }
+      case "UPDATE_GROUP": {
+        const newGroups = [...state.groups]
+        const groupToUpdate = newGroups.findIndex(group => group.id === action.payload.id)
+        newGroups[groupToUpdate] = action.payload;
 
+        return {
+          ...state,
+          groups: newGroups,
+        }
+      }
+    }
+      
     return state
 }
-

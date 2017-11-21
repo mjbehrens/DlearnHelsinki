29bf498e529c76a8570bdfdc8fda0ff10c3c1d8e
mjbehrens <33420821+mjbehrens@users.noreply.github.com>
2017-11-21 14:28:18 +0100

    adding swap between competence and progression to competence wall

--- src/pages/CompetenceWall.js
+++ src/pages/CompetenceWall.js
@@ -14,7 +14,7 @@ class CompetenceWall extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            view: "graph",
+            view: "competence",
         };
     }
 
@@ -31,11 +31,12 @@ class CompetenceWall extends Component {
         return (
             <div className="container">
                 <h1>Competence Wall</h1>
+                <br/>
                 <div className="btn-group btn-group-lg" role="group">
-                    <button className="btn btn-primary" onClick={() => this.selectView('graph')}>Graph</button>
+                    <button className="btn btn-primary" onClick={() => this.selectView('competence')}>Competence</button>
                     <button className="btn btn-primary" onClick={() => this.selectView('progression')}>Progression</button>
                 </div>
-                {this.state.view === 'graph' ? <p>render graph</p> : <p>render progression</p>}
+                {this.state.view === 'competence' ? <p>render competence</p> : <p>render progression</p>}
             </div>
         );
     }

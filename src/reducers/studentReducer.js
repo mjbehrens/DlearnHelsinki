4c0486d7794fd0b8388d2e002f92a9da3d17713f
Mathieu Rollet <mathieu.rollet@insa-lyon.fr>
2017-11-21 16:05:42 +0200

    Update student reducer

--- src/reducers/studentReducer.js
+++ src/reducers/studentReducer.js
@@ -1,32 +1,42 @@
-export default function reducer(state={
+const studentInitState = {
     students: [],
-  }, action) {
+}
+
+export default function reducer(state=studentInitState, action) {
 
     switch (action.type) {
-      case "ADD_GROUP": {
+      case "SET_ALL_STUDENTS": {
+	return {
+	  ...state,
+	  students: action.payload,
+	}
+      }
+      case "DELETE_ALL_STUDENTS": {
+        return studentInitState
+      }
+      case "ADD_STUDENT": {
         return {
           ...state,
-          groups: [...state.groups, action.payload],
+          students: [...state.students, action.payload],
         }
       }
-      case "UPDATE_TWEET": {
-        const { id, text } = action.payload
-        const newTweets = [...state.tweets]
-        const tweetToUpdate = newTweets.findIndex(tweet => tweet.id === id)
-        newTweets[tweetToUpdate] = action.payload;
+      case "DELETE_STUDENT": {
+        return {
+          ...state,
+          students: state.students.filter(student => student.id !== action.payload),
+        }
+      }
+      case "UPDATE_STUDENT": {
+        const newStudents = [...state.students]
+        const studentToUpdate = newStudents.findIndex(student => student.id === action.payload.id)
+        newStudents[studentToUpdate] = action.payload;
 
         return {
           ...state,
-          tweets: newTweets,
-        }
-      }
-      case "DELETE_GROUP": {
-        return {
-          ...state,
-          groups: state.groups.filter(group => group.id !== action.payload),
+          students: newStudents,
         }
       }
     }
-
+      
     return state
 }

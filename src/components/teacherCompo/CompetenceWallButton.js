53817b30be9cc442118feab6382d6d6b918d63e6
mjbehrens <33420821+mjbehrens@users.noreply.github.com>
2017-11-21 11:43:27 +0100

    create button for competence wall

---
+++ src/components/teacherCompo/CompetenceWallButton.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+import iconGrpManagment from "../../res/icons/manage_groups.svg";
+import { ROUTES } from '../../constants.js';
+
+const style = {
+    padding: 50,
+    margin: 50,
+    textAlign: 'center',
+    background: 'green'
+};
+
+
+class CompetenceWallButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "Competence Wall",
+            picture: iconGrpManagment,
+        }
+    }
+
+    onClickGrpManagment = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <Link to={ROUTES.COMPETENCE_WALL}>
+                    <img className="card-img-top teacher-card-img" src={this.state.picture} width="100" height="100"
+                        onClick={this.onClickSurvey}
+                        alt="survey icon" />
+                    <div className="card-body">
+                        <h4 className="card-title">{this.state.text}</h4>
+                    </div>
+                </Link>
+            </div>
+        )
+    }
+
+}
+
+export default CompetenceWallButton;

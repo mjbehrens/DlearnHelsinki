bb1dcf88b7acc52fd443ad4a9c584574b7e7da2a
mjbehrens <33420821+mjbehrens@users.noreply.github.com>
2017-12-11 14:38:28 +0100

    changes the swap compo. Buttons change color

--- src/pages/CompetenceWall.js
+++ src/pages/CompetenceWall.js
@@ -20,14 +20,28 @@ class CompetenceWall extends Component {
         super(props);
         this.state = {
             view: "competence",
+            buttonState1: "btn btn-primary",
+            buttonState2: "btn btn-secondary"
         };
     }
 
     selectView = (newView) => {
-        this.setState({
-            ...this.state,
-            view: newView,
-        })
+
+        if(newView=="competence"){
+            this.setState({
+                ...this.state,
+                buttonState1: "btn btn-primary",
+                buttonState2: "btn btn-secondary",
+                view: newView,
+            })
+        }else {
+            this.setState({
+                ...this.state,
+                buttonState1: "btn btn-secondary",
+                buttonState2: "btn btn-primary",
+                view: newView,
+            })
+        }
     }
 
 
@@ -54,12 +68,13 @@ class CompetenceWall extends Component {
                 <h1>Competence Wall</h1>
                 <br />
                 <div className="btn-group btn-group-lg" role="group">
-                    <button className="btn btn-primary" onClick={() => this.selectView('competence')}>Competence</button>
-                    <button className="btn btn-primary" onClick={() => this.selectView('progression')}>Progression</button>
+                    <button className={this.state.buttonState1} onClick={() => this.selectView('competence')}>Competence</button>
+                    <button className={this.state.buttonState2} onClick={() => this.selectView('progression')}>Progression</button>
                 </div>
                 <br /><br /><br />
                 {this.state.view === 'competence' ? <CompetenceRender /> : <ProgressionRender />}
-                {//<DataCollector parameters={parameters} name="toto" color="black"/>
+                {
+                    
                 }
                 <br />
             </div>

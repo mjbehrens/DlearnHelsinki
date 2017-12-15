bbb14a3b50f3ac999f2c91813fd3b1f807d2d72c
mjbehrens <33420821+mjbehrens@users.noreply.github.com>
2017-12-15 13:28:17 +0100

    added remove and add stuff for log

--- src/components/teacherCompo/CompetenceRender.js
+++ src/components/teacherCompo/CompetenceRender.js
@@ -12,39 +12,59 @@ function mapStateToProps(store) {
 class CompetenceRender extends Component {
 
     constructor(props) {
+
         super(props);
         this.state = {
-        
+            data_competence: ["teachers/1/classes/2/gruops/1/students/3", "teachers/1/classes/2/gruops/2/students/2", "teachers/1/classes/2/gruops/1/students/1"],            
         };
+    }
+
+    helloWolrd = function () {
+        console.log("hello")
+    }
+
+    deleteFromLog = function (id) {
+        let temp = [] 
+        this.state.data_competence.forEach(function(element) {
+            if(element != id){
+                temp.push(element)
+            }
+        }, this);
+
+        this.setState({
+            data_competence : temp
+        })
+
     }
 
 
 
     render() {
 
+
         let parameters = [{
-            name : "student 8 - hlqejqe",
+            name: "student 8 - hlqejqe",
             teachers: this.props.user.id,
             students: 8,
             classes: 1,
             groups: null,
             surveys: 27,
-          },
-          {
-            name : "1 - class",
+        },
+        {
+            name: "1 - class",
             teachers: this.props.user.id,
             students: null,
             classes: 1,
             groups: null,
             surveys: 27,
-          }
+        }
         ]
 
         return (
             <div className="container">
                 <div className="row">
-                    <div className="left-align col-sm-3"><CompetenceWallLog /></div>
-                    <div className="col-sm-9"><SpiderGraph2 parameters={parameters} name="toto" color="black"/></div>
+                    <div className="left-align col-sm-3"><CompetenceWallLog myFunction={this.helloWolrd} data_competence={this.state.data_competence} functionDelete={this.deleteFromLog.bind(this)}/></div>
+                    <div className="col-sm-9"><SpiderGraph2 parameters={parameters} name="toto" color="black" /></div>
                 </div>
             </div>
         );
